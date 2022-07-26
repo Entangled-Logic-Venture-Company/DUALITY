5f06792dcc2604c67962759ac18437185326ea9f
Rebeka Fox <rebekafox@outlook.com>
2022-07-26 08:11:45 -0700

    Kernels

---
+++ KernelsST/ContainerWindow.xaml.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "ContainerFrame.xaml.h"
+#include "ContainerUI.xaml.h"
+
+#include "ContainerWindow.g.h"
+
+namespace winrt::KernelsST::implementation
+{
+    struct ContainerWindow : ContainerWindowT<ContainerWindow>
+    {
+        ContainerWindow();
+
+        void Window_Activated(winrt::Windows::Foundation::IInspectable const& sender, 
+            winrt::Microsoft::UI::Xaml::WindowActivatedEventArgs const& args);
+
+    private:
+        HWND                                        hWnd{ nullptr };
+
+        winrt::com_ptr<IWindowNative>               nativeWindow{ nullptr };
+
+        Microsoft::UI::Windowing::AppWindow         oApplicationWindow{ nullptr };
+
+        winrt::KernelsST::ContainerFrame      oApplicationFrame{ nullptr };
+
+        HV3DHEADER::HV3DHeader                      oApplicationHeader{ nullptr };
+
+    };
+
+}
+
+namespace winrt::KernelsST::factory_implementation
+{
+    struct ContainerWindow : ContainerWindowT<ContainerWindow, implementation::ContainerWindow>
+    {
+    };
+}
